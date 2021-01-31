277cb9a891e2627f999b6e1e805bf4b7fbe712c5
오해인 <hoh3@mail.csuchico.edu>
2021-02-01 00:32:05 +0900

    section 3 : appconfig refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,12 +12,22 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
     // appliction 환경 구성은 여기서 다 한다.
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해서 객체가 들어간다 :: 생성자 주입
+    // 현재 나의 어플리케이션에서 뭐를 어떻게 쓸건지 한 눈에 볼 수 있다
+
+    // ex. 이 어플리케이션에서 memberservice는 memberserviceimpl을 쓸거다!
+    public MemberService memberService() { // ctrl + alt + m
+        return new MemberServiceImpl(memberRepository()); // 생성자를 통해서 객체가 들어간다 :: 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
