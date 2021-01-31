15a2d2755a8e51de126129fa16b05b593ee05e59
오해인 <hoh3@mail.csuchico.edu>
2021-02-01 01:22:12 +0900

    section 3 : 객체 지향 원래 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,32 +2,41 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 애플리케이션 설정/구성정보
 public class AppConfig {
     // appliction 환경 구성은 여기서 다 한다.
 
     // 현재 나의 어플리케이션에서 뭐를 어떻게 쓸건지 한 눈에 볼 수 있다
 
     // ex. 이 어플리케이션에서 memberservice는 memberserviceimpl을 쓸거다!
+    @Bean // 각 메서드에 적어준다. 이러면 다 각각 스프링컨테이너에 올라간다.
     public MemberService memberService() { // ctrl + alt + m
         return new MemberServiceImpl(memberRepository()); // 생성자를 통해서 객체가 들어간다 :: 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 할인정책을 변경 할 때 이 부분만 변경하면 된다.
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService(); // memberServiceImpl이 들어가 있다.
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // memberServiceImpl이 들어가 있다.
+
+        // annotation 기반으로 config
+        ApplicationContext applicationContext
+                = new AnnotationConfigApplicationContext(AppConfig.class); // appconfig에 있는 환경 설정 정보를 가지고 spring 컨테이너에 다 넣어서 관리 해준다.
+        // spring container에 bean에 있는 애들이 메서드 이름이 key값, 객체를 value값으로 spring container에 올라간다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 메서드 이름으로 등록되어 있다.
+
         Member member = new Member(1L, "memberA", Grade.VIP); // ctrl + alt + V
         memberService.join(member);
 
